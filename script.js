c34b655e95e31c92b8b69ab3f8e7bad3f547827f
jason cooper <cooper_jason@bah.com>
2025-10-19 19:37:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function openMazeTab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("mzTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+
+function toggleDisplay(eleId){
+    if (!!document.getElementById(eleId)){
+        let currentDisplay = document.getElementById(eleId).style.display;
+        let updatedDisplay = currentDisplay == "block" ? "none" : "block";
+        document.getElementById(eleId).style.display = updatedDisplay;
+    }
+}

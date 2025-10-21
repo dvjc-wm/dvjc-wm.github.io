e9ba1338126a071a6cb23767f245e309ab9853ac
jason cooper <cooper_jason@bah.com>
2025-10-21 18:57:04 -0400

    changes for 2025-10-21 - auto-builds display of version history when tab is clicked

--- script.js
+++ script.js
@@ -30,8 +30,28 @@ async function refreshVersionHistory(){
             throw new Error(`Response status: ${response.status}`);
         }
 
-        const result = await response.json();
-        console.log(result);
+        const versionHistory = await response.json();
+        
+        let tableElement = document.getElementById('versionHistoryTable');
+        if (!!tableElement){
+            Object.keys(versionHistory).forEach( versionKey => {
+                let record = versionHistory[versionKey];
+                var tr = document.createElement("tr");
+                var td1 = document.createElement("td");
+                td1.classList = ['versionInfoC1'];
+                td1.createTextNode = record.version;
+                tr.appendChild(td1);
+                var td2 = document.createElement("td");
+                td2.classList = ['versionInfoC2'];
+                td2.createTextNode = record.time;
+                tr.appendChild(td2);
+                var td3 = document.createElement("td");
+                td3.classList = ['versionInfoC3'];
+                td3.createTextNode = record.text;
+                tr.appendChild(td3);
+                tableElement.appendChild(tr);
+            });
+        }
     } catch (error) {
         console.error(error.message);
     }

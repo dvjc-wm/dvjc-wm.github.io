7f5de48b47caa9cffbc59dc55638584c442c58c5
jason cooper <cooper_jason@bah.com>
2025-10-22 12:13:07 -0400

    changes for 2025-10-22 - generating a visible maze (static)

--- script.js
+++ script.js
@@ -110,3 +110,66 @@ function removeCommand(buttonId){
         li.parentNode.removeChild(li);
     }
 }
+
+/* polar - north, west, south, east
+// walls: 1 - north
+          2 - west
+          4 - south
+          8 - east
+    so, for instances, west + south = 6
+//*/          
+let mazeBoard = [
+    // meta - width, height, exit { position, direction }, entrance (position, direction)
+    [ 5, 5, { position: { x: 2, y: 0 }, direction: 1 },  { position: { x: 4, y: 4 }, direction: 4 } ],
+    // 
+    [   // example 5x5
+        [  3,  4,  5,  1,  9],
+        [ 10,  3,  5,  8, 14],
+        [ 14,  6,  1,  6,  9],
+        [ 11,  3, 12,  3, 12],
+        [  6,  4, 13,  6,  9],
+    ]
+];
+function drawMazeBoard(){
+    let ele = document.getElementById('mazeBoard');
+    if (!!ele){
+        ele.innerHTML = "";
+        let table = document.createElement('table');
+        table.style = "text-align: center; border-collapse: collapse;";
+        let tbody = document.createElement('tbody');
+
+        let x = mazeBoard[0][0];
+        let y = mazeBoard[0][1];
+        for (let j = 0; j < y; j++){
+            let tr = document.createElement('tr');
+            tr.style = "height: 2em;";
+            for (let i = 0; i < x; i++){
+                let td = document.createElement("td");
+                let text = mazeBoard[1][j][i];
+                td.style.width = "2em";
+                td.style.borderCollapse = "collapse";
+                if (text >= 8) {
+                    td.style.borderRight = "2px solid black";
+                    text -= 8;
+                }
+                if (text >= 4) {
+                    td.style.borderBottom = "2px solid black";
+                    text -= 4;
+                }
+                if (text >= 2) {
+                    td.style.borderLeft = "2px solid black";
+                    text -= 2;
+                }
+                if (text >= 1) {
+                    td.style.borderTop = "2px solid black";
+                    text -= 1;
+                }
+                //td.innerText = text;
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
+        }
+        table.appendChild(tbody);
+        ele.appendChild(table);
+    }
+}
